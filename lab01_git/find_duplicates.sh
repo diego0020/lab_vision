bbccc391d991a7f29e92f6653b35444ab9ea6290
careyes1787 <carlosreyes@MacBook-Pro-de-Carlos.local>
2016-02-18 11:31:24 -0500

    changes to the file

---
+++ lab01_git/find_duplicates.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+declare -A arr
+shopt -s globstar
+
+for file in **; do
+  [[ -f "$file" ]] || continue
+
+  read cksm _ < <(md5sum "$file")
+  if ((arr[$cksm]++)); then 
+    echo "rm $file"
+  fi
+done
+# obtenido de http://superuser.com/questions/386199/how-to-remove-duplicated-files-in-a-directory

decddd50e435368387fdf755d6df61f0d8e165c5
Julian Alexander Martinez Logreira <julialex@MacBook-Pro-de-Julian.local>
2016-02-18 03:21:42 -0500

    changes to the file

--- lab01_git/find_duplicates.sh
+++ lab01_git/find_duplicates.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+
+# Declaracion de variables
 declare -A arr
 shopt -s globstar
 
+# Ciclo for iterativo
 for file in **; do
   [[ -f "$file" ]] || continue
 
+  # Revisa el checksum de cada archivo
   read cksm _ < <(md5sum "$file")
   if ((arr[$cksm]++)); then 
+    # Si los checksum son iguales imprime el nombre del archivo, es decir los archivos son el mismo.
     echo "rm $file"
   fi
 done

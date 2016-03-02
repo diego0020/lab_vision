ba90e54eec76c39ff480a80e0c19cb461d79c649
Carlos Arturo Acosta Herrera <CarlosAAcostaH@users.noreply.github.com>
2016-03-02 09:53:33 -0500

    Update lab3_hybrid.m

--- lab3_hybrid/lab3_hybrid.m
+++ lab3_hybrid/lab3_hybrid.m
@@ -1,20 +1,12 @@
-clear all;
-close all;
-I1=imread('./fotos/NuevaCapilla.jpg');
-I2=imread('./fotos/ViejaCapilla.jpg');
-a=40;
-b=40;
-F1=fspecial('gaussian',floor(2*a+1)*[1 1],a*0.5);
-F2=fspecial('gaussian',floor(2*b+1)*[1 1],b*0.5);
-G1=imfilter(I1,F1);
-G2=imfilter(I2,F2);
-check=true;
-% subplot (1,2,1); imshow(I1);
-% subplot (1,2,2); imshow(G1);
-if check
-    imshow(G1+(I2-G2));
-    imwrite(G1+(I2-G2),'./fotos/HibridaCapilla.jpg')
-else
-    imshow(G2+(I1-G1));
-    imwrite(G2+(I1-G1),'./fotos/HibridaCapilla.jpg')
-end
+clear all; # Clear all variables
+close all; # Close all open figures
+I1=imread('./fotos/NuevaCapilla.jpg'); # Create I1 as a 2016 chapel photo after modificated with GIMP(R)
+I2=imread('./fotos/ViejaCapilla.jpg'); # Create I2 as a 1969 chapel photo after modificated with GIMP(R)
+a=40; # Set an a parameter of the filter to apply to I1
+b=40; # Set a b parameter of the filter to apply to I2
+F1=fspecial('gaussian',floor(2*a+1)*[1 1],a*0.5); # Create a low-pass filter F1 that will be apply to I1
+F2=fspecial('gaussian',floor(2*b+1)*[1 1],b*0.5); # Create a low-pass filter F1 that will be apply to I2
+G1=imfilter(I1,F1); # Apply the filter F1 to I1 and save as G1
+G2=imfilter(I2,F2); # Apply the filter F2 to I2 and save as G2
+imshow(G1+(I2-G2)); # Show the hybrid image
+imwrite(G1+(I2-G2),'./fotos/HibridaCapilla.jpg') # Save the hybrid image created in a jpg file
